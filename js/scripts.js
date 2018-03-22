738f040c93c35286a1c2d9046d3a8efdc147e5d7
Aaron Akbari-Mort and kelsey guerra <aaron@>
2018-03-22 13:31:32 -0700

    create working functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,25 @@
 $(document).ready(function() {
   $("form#celebrity").submit(function(event) {
-    var flower = parseInt($("select#flower").val());
-    var movie = parseInt($("select#movie").val());
+    var flower = parseInt($("#flower").val());
+    var movie = parseInt($("#movie").val());
     var animal = parseInt($("input:radio[name=cat]:checked").val());
     var score = flower + movie + animal;
 
     console.log("hello");
 
-    $(".answer").show();
-    if (answer <=5) {
+    $("#answer").show();
+    if (score <=3) {
       console.log("streep should show");
-      $("")
+      $("#streep").show();
+    } else if (score <=5) {
+      console.log("gadot should show");
+      $("#gadot").show();
+    } else {
+      $("#clooney").show();
     }
+
+    $("form#celebrity").hide();
+    event.preventDefault();
+
   });
 });
